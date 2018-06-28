4a94f687f529cb7f6f7fa3cd09a46704ec70deb8
AlexD7 <alex.stellas@hotmail.com>
2018-06-29 00:03:29 +0100

    Added Backend

---
+++ Backend/game_of_codes_API/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\AlexD7\Desktop\Master's Projects\_Master's Thesis\game-of-codes\Backend\game_of_codes_API\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
